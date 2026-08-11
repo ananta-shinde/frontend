3e8e02fdf4825ea9b886ef9bf5e8571659a57fc0
aShinde <ananta.shinde@honeywellcom>
2026-08-11 16:18:36 +0530

    11/08/2026

--- src/pages/ProductListing.jsx
+++ src/pages/ProductListing.jsx
@@ -15,9 +15,40 @@ const ProductListing = () => {
     
     },[])
 
+    const handlePriceFilterChange = (event)=>{
+         var value = event.target.value
+        
+        if(value == "asc"){
+            var filteredProducts = products.sort((a,b)=>a.price-b.price)
+            setproducts(filteredProducts)
+              console.log(filteredProducts)
+        }else if(value == "desc"){
+
+        }else{
+
+        }
+    }
+
  
     return (  
         <>
+        <div className="row">
+        <div className="col p-4">
+            <h4 className="my-4">filters</h4>
+        </div>
+        <div className="p-4 col-9">
+          <div className="d-flex justify-content-between py-4">
+            <h4>Product List</h4>
+            <div className="d-flex">
+                <select className="form-select  mx-2" onChange={handlePriceFilterChange}>
+                    <option value="">-- select sort filter --</option>
+                    <option value="asc">price low to high</option>
+                    <option value="desc">price high to low</option>
+                </select>
+                <input className="  form-control"  type="text" placeholder="search product here"/>
+            </div>
+          </div>
+          
           {
             products.map(product=>(
                <div className="card my-2 p-4">
@@ -45,6 +76,8 @@ const ProductListing = () => {
                </div>
             ))
           }
+        </div>
+        </div>
         </>
     );
 }
