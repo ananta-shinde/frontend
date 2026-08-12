413c6b3f4c0839b8f5cd3bc229627890ad909018
aShinde <ananta.shinde@honeywellcom>
2026-08-12 16:38:35 +0530

    12/08/2026

--- src/pages/ProductListing.jsx
+++ src/pages/ProductListing.jsx
@@ -1,9 +1,14 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 const ProductListing = () => {
 
     // create an empty array to save products
+    const [originalproducts,setOriginalProducts] = useState([]);
     const [products,setproducts] = useState([]);
+    const [isIntersecting, setIsIntersecting] = useState(false);
+    
+    const productListRef = useRef(null);
+
 
 
     useEffect(()=>{
@@ -11,32 +16,64 @@ const ProductListing = () => {
         .then(res => res.json())
         .then(res =>{
                 setproducts(res.products)
+                setOriginalProducts(res.products)
             });
-    
+        
     },[])
+
+   
+    useEffect(()=>{
+            const observer = new IntersectionObserver((entries)=>{
+                console.log(entries)
+                if(entries[0].isIntersecting){
+                    console.log("somthing happening")
+                }
+             });
+
+     // 5. Start tracking the DOM element
+        if (productListRef.current) {
+        observer.observe(productListRef.current);
+        }
+    },[isIntersecting])
+   
+
 
     const handlePriceFilterChange = (event)=>{
          var value = event.target.value
         
         if(value == "asc"){
-            var filteredProducts = products.sort((a,b)=>a.price-b.price)
+            var filteredProducts = [...products].sort((a,b)=>a.price-b.price)
             setproducts(filteredProducts)
-              console.log(filteredProducts)
+             
         }else if(value == "desc"){
-
+            var filteredProducts = [...products].sort((a,b)=>b.price-a.price)
+            setproducts(filteredProducts)
         }else{
-
+            setIsIntersecting(true)
         }
     }
+
+    const handleSearch = (event)=>{
+
+        var value = event.target.value
+        if(value != ""){
+            setproducts([...originalproducts].filter(product=>(product.title.toLowerCase().includes(value.toLowerCase().trim()))))
+        }else{
+            setproducts(originalproducts)
+        }
+        
+    }
+
+
 
  
     return (  
         <>
         <div className="row">
-        <div className="col p-4">
+        <div className="col-3 p-4 position-fixed">
             <h4 className="my-4">filters</h4>
         </div>
-        <div className="p-4 col-9">
+        <div className="p-4 offset-3 col-9" >
           <div className="d-flex justify-content-between py-4">
             <h4>Product List</h4>
             <div className="d-flex">
@@ -45,7 +82,7 @@ const ProductListing = () => {
                     <option value="asc">price low to high</option>
                     <option value="desc">price high to low</option>
                 </select>
-                <input className="  form-control"  type="text" placeholder="search product here"/>
+                <input className="  form-control"  type="text" onChange={handleSearch} placeholder="search product here"/>
             </div>
           </div>
           
@@ -76,6 +113,7 @@ const ProductListing = () => {
                </div>
             ))
           }
+          <p ref={productListRef}> more products</p>
         </div>
         </div>
         </>
